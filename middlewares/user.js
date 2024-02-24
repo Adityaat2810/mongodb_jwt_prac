bb2849d609aac66db8657304b57d78fdb345c72b
aditya <adityasainics10@gmail.com>
2024-02-24 17:17:58 +0530

    added user middleware

---
+++ middlewares/user.js
@@ -0,0 +1,21 @@
+const jwt =require('jsonwebtoken');
+const secret =require('../index');
+
+function userMiddleware(req,res,next){
+    const token = req.headers.authorization;
+    //Bearer jwt
+    const words =token.split(" ");
+    const jwtToken =words[1];
+
+    const decodedValue=jwt.verify(jwtToken,secret);
+    if(decodedValue.username){
+        next();
+    }else{
+        res.status(403).json({
+            message:"you are not authenticated"
+        })
+    }
+}
+
+module.exports = userMiddleware;
+
