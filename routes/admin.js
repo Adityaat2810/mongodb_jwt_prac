648c80c23e9d23dfa8d79ba17b3e929809ebe6d4
aditya <adityasainics10@gmail.com>
2024-02-24 18:27:06 +0530

    added admin authentication logic

---
+++ routes/admin.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const adminMiddleware = require('../middleware/admin');
+const router = express.Router();
+const {Admin} =require('../db/index');
+const {Course} =require('../db/index');
+const jwt =require('jsonwebtoken'); 
+const JWT_SECRET = require('..');
+    
+    
+router.post('/signup',async (req,res)=>{
+
+  try{
+    const username =req.body.username;
+    const password =req.body.password;
+
+    //check if user with username is already exists
+    await Admin.create({
+        username,
+        password
+    })
+
+    res.json({
+        message:'admin created successfully'
+    })
+  }catch(error){
+    console.log('someting went wrong in admin signup route');
+    throw{error};
+}
+
+});
+
+
+router.post('/signin',async (req,res)=>{
+    //Implement admin signup logic
+    const username  =req.body.username;
+    const password  =req.body.password;
+
+    const isValidated = await User.find({
+        username,
+        password
+    })
+    
+    if(isValidated){
+        const token =jwt.sign({
+            username
+        },JWT_SECRET);
+         
+        res.json({
+            token
+        })
+    }else{
+        res.status(411).json({
+            message:'Incorrect credentiald'
+        })
+    }
+
+})
+
+module.exports = router
+
